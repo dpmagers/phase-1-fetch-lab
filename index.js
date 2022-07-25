d54c12f74b02bd133eb991fe405ed8af5c9caf51
Daniel Magers <daniel.magers@gmail.com>
2022-07-25 12:48:23 -0500

    commit tree 207b81575cffea418f7192ef901962595f4de03a

--- index.js
+++ index.js
@@ -1,17 +1,178 @@
-function fetchBooks() {
-  // To pass the tests, don't forget to return your fetch!
-  
+document.addEventListener('DOMContentLoaded', ()=>{
+
+fetchBooks()
+
+})
+
+// function fetchBooks(){
+// fetch("https://anapioficeandfire.com/api/books")
+// .then(res => res.json())
+// .then(books => renderBooks(books))
+// }
+function fetchBooks(){
+fetch("https://anapioficeandfire.com/api/books")
+.then(res => res.json())
+.then(books => renderBooks(books))
 }
 
-function renderBooks(books) {
-  const main = document.querySelector('main');
+function renderBooks(books){
+  //grab html element 
+  const main = document.querySelector('main')
   books.forEach(book => {
-    const h2 = document.createElement('h2');
-    h2.innerHTML = book.name;
-    main.appendChild(h2);
-  });
+    const h2 = document.createElement('h2')
+    h2.textContent = book.name
+    main.append(h2, img)
+
+
+  })
+  //iterate over the array and pull out each book
+  //create element for each book
+  //append element to the dom 
 }
 
-document.addEventListener('DOMContentLoaded', function() {
-  fetchBooks();
-});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function fetchBooks() {
+//   fetch ("https://anapioficeandfire.com/api/books") 
+//     .then(resp => resp.json())
+//     .then(json => renderBooks(json))
+// }
+  
+// //json is the parameter of this anonymous function. passed as an argument to renderBooks
+
+
+
+
+
+// // (https://anapioficeandfire.com/api/books) 
+// //   .then ((resp) = resp.json())
+// //   .then (json) = > console.log("hello")
+// // }
+
+// https://anapioficeandfire.com/api/books
+
+//   // To pass the tests, don't forget to return your fetch!
+
+
+// // fetch("https://anapioficeandfire.com/api/books")
+// //   .then((resp) => resp.json())
+// //   .then((json) => console.log(json));
+
+
+// // fetch("string representing a URL to a data source")
+// //   .then(function (response) {
+// //     return response.json();
+// //   })
+// //   .then(function (data) {
+// //     // Use the data from the response to do DOM manipulation
+// //   });
+
+
+// function renderBooks(books) {
+//   const main = document.querySelector('main');
+//   books.forEach(book => {
+//     const h2 = document.createElement('h2');
+//     h2.innerHTML = book.name;
+//     main.appendChild(h2);
+//   });
+// }
+
+
+
+// document.addEventListener('DOMContentLoaded', function() {
+//   fetchBooks();
+// });
